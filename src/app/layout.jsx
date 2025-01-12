42cc8f14df26a7b5567fdf4717f4a93e6790fc87
doshrun <kaveh.peak12@gmail.com>
2025-01-12 18:44:26 +0330

    Update layout.jsx
    add footer in layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,3 +9,16 @@ export default async function RootLayout({ children }) {
     </html>
   );
 }
+import '../../styles/globals.css';
+import Footer from './Footer';
+
+export default async function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
+}
