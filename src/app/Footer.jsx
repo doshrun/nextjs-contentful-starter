6b7986a6ad6c4591d6f7bb14e589f63fcfd217b9
doshrun <kaveh.peak12@gmail.com>
2025-01-12 18:40:48 +0330

    Create Footer.jsx
    add footer in app footer.jsx

---
+++ src/app/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div className="logos">
+                <a 
+                    referrerPolicy='origin' 
+                    target='_blank' 
+                    href='https://trustseal.enamad.ir/?id=567151&Code=l9edf3vbBzI61Adx3apmENARvUxozD4x'
+                >
+                    <img 
+                        src='https://trustseal.enamad.ir/logo.aspx?id=567151&Code=l9edf3vbBzI61Adx3apmENARvUxozD4x' 
+                        alt='لوگو اینماد' 
+                        style={{ cursor: 'pointer', width: '100px', height: 'auto' }} 
+                    />
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
