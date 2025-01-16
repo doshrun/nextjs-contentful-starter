ce289c08922bd52e33201c28215386f018361df4
doshrun <kaveh.peak12@gmail.com>
2025-01-16 12:01:28 +0330

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,4 @@
 import '../../styles/globals.css';
-
-export default async function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
-}
-import '../../styles/globals.css';
 import Footer from './Footer';
 
 export default async function RootLayout({ children }) {
