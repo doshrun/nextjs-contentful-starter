b6d4c8049abd15bd0bd81629324c6ca5e0edc3af
doshrun <kaveh.peak12@gmail.com>
2025-01-20 22:24:41 +0330

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div style="position:absolute; width:100%;height:131px;left:-45%; top:10px;">
+                <a 
+                    referrerPolicy='origin' 
+                    target='_blank' 
+                    href='https://trustseal.enamad.ir/?id=567151&Code=l9edf3vbBzI61Adx3apmENARvUxozD4x'
+                >
+                    <img 
+                        src='https://trustseal.enamad.ir/logo.aspx?id=567151&Code=l9edf3vbBzI61Adx3apmENARvUxozD4x' 
+                        alt='لوگو اینماد' 
+                        style={{ cursor: 'pointer', width: '100px', height: 'auto' }} code='l9edf3vbBzI61Adx3apmENARvUxozD4x'
+                    />
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
