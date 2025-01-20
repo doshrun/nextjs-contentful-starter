30a9d118a0614db0b97c442fcdff0a30cb499241
doshrun <kaveh.peak12@gmail.com>
2025-01-20 22:28:40 +0330

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,19 @@ const themeClassMap = {
 
 export const Hero = (props) => {
   return (
-    <Footer/>
+    <div style="position:absolute; width:100%;height:131px;left:-45%; top:10px;">
+                <a 
+                    referrerPolicy='origin' 
+                    target='_blank' 
+                    href='https://trustseal.enamad.ir/?id=567151&Code=l9edf3vbBzI61Adx3apmENARvUxozD4x'
+                >
+                    <img 
+                        src='https://trustseal.enamad.ir/logo.aspx?id=567151&Code=l9edf3vbBzI61Adx3apmENARvUxozD4x' 
+                        alt='لوگو اینماد' 
+                        style={{ cursor: 'pointer', width: '100px', height: 'auto' }} code='l9edf3vbBzI61Adx3apmENARvUxozD4x'
+                    />
+                </a>
+            </div>
     <div className="px-6 py-16 bg-gray-100 sm:px-12 sm:py-24" data-sb-object-id={props.id}>
       <div className={`max-w-6xl mx-auto flex flex-col gap-12 md:items-center ${themeClassMap[props.theme] ?? themeClassMap['imgRight']}`}>
         <div className="w-full max-w-xl mx-auto flex-1">
